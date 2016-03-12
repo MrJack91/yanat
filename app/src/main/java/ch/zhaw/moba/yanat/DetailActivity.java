43025ecf59257e048e55ccdb47dc97640cf717f1
miltom <mili92@gmail.com>
2016-03-12 21:40:00 +0100

    created measure point detail layout

--- app/src/main/java/ch/zhaw/moba/yanat/DetailActivity.java
+++ app/src/main/java/ch/zhaw/moba/yanat/DetailActivity.java
@@ -1,7 +1,20 @@
 package ch.zhaw.moba.yanat;
 
+import android.content.DialogInterface;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import ch.zhaw.moba.yanat.domain.model.Project;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +22,40 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+
+        Button button = (Button) findViewById(R.id.create_measure_point);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(DetailActivity.this);
+                LayoutInflater inflater = DetailActivity.this.getLayoutInflater();
+                final View view = inflater.inflate(R.layout.dialog_measure_point, null);
+
+
+                builder.setView(view);
+                builder.setTitle("Messpunkt");
+                // Add action buttons
+                builder.setPositiveButton("Erstellen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                        .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        })
+                ;
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
+
     }
+
 }
