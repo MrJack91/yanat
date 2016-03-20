7ac229beb469813bb2688a042619a3a93b84db37
miltom <mili92@gmail.com>
2016-03-20 15:15:16 +0100

    Point layout changed, show single point details

--- app/src/main/java/ch/zhaw/moba/yanat/DetailActivity.java
+++ app/src/main/java/ch/zhaw/moba/yanat/DetailActivity.java
@@ -7,11 +7,14 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -22,10 +25,14 @@ import ch.zhaw.moba.yanat.domain.model.Point;
 import ch.zhaw.moba.yanat.domain.model.Project;
 import ch.zhaw.moba.yanat.domain.repository.PointRepository;
 import ch.zhaw.moba.yanat.domain.repository.ProjectRepository;
+import ch.zhaw.moba.yanat.view.PointAdapter;
+import ch.zhaw.moba.yanat.view.ProjectAdapter;
 
 public class DetailActivity extends AppCompatActivity {
 
     public ProjectRepository projectRepository = new ProjectRepository(DetailActivity.this);
+    public PointRepository pointRepository = null;
+    private PointAdapter pointAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,23 +47,15 @@ public class DetailActivity extends AppCompatActivity {
                 ProjectContract.ProjectEntry.COLUMN_NAME_ID + " LIKE ?",
                 new String[]{String.valueOf(projectId)}
         );
-        PointRepository pointRepository = projects.get(0).getPointRepository(this);
-
-        List<Point> points = pointRepository.findAll();
-        int i;
-        for (i = 0; i < points.size(); i++) {
-            Log.v("YANAT", Float.toString(points.get(i).getHeight()));
-        }
-
-        Point point = new Point();
-        point.setHeight(i);
-        pointRepository.add(point);
+        pointRepository = projects.get(0).getPointRepository(this);
 
 
+      //  final Point point = new Point();
+      //  point.setHeight(i);
+      //  pointRepository.add(point);
 
-
-        Button button = (Button) findViewById(R.id.create_measure_point);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button createMeasure = (Button) findViewById(R.id.create_measure_point);
+        createMeasure.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -64,14 +63,47 @@ public class DetailActivity extends AppCompatActivity {
                 LayoutInflater inflater = DetailActivity.this.getLayoutInflater();
                 final View view = inflater.inflate(R.layout.dialog_measure_point, null);
 
-
+                List<Point> points = getPoints();
+                Point lastPoint = points.get(0);
                 builder.setView(view);
                 builder.setTitle("Messpunkt");
-                // Add action buttons
+
+
+                final int pointsCount = points.size();
+                Log.v("YANAT", "Points size: " + pointsCount);
+
+
+                ((TextView)view.findViewById(R.id.input_measure_point_comment)).setText(lastPoint.getComment());
+                ((TextView)view.findViewById(R.id.input_measure_point_height)).setText("" + lastPoint.getHeight());
+
+                // TOOO anschauen
+                ((CheckBox)view.findViewById(R.id.ground_floor)).setChecked(lastPoint.isGroundFloor());
+                ((CheckBox)view.findViewById(R.id.meter_above_sea)).setChecked(lastPoint.isAbsolute());
+
+
                 builder.setPositiveButton("Erstellen", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
+                        EditText pointComment = (EditText) view.findViewById(R.id.input_measure_point_comment);
+                        EditText pointHeight = (EditText) view.findViewById(R.id.input_measure_point_height);
+                        CheckBox groundFloor = (CheckBox) view.findViewById(R.id.ground_floor);
+                        CheckBox meterAboveSea = (CheckBox) view.findViewById(R.id.meter_above_sea);
 
+                        Point point = new Point();
+                        point.setComment(pointComment.getText().toString());
+                        // TODO testen ob man Buchstaben eingeben kann
+                        point.setHeight(Float.parseFloat(pointHeight.getText().toString()));
+
+                        point.setTitle("A");
+                        point.setPosX(1);
+                        point.setPosY(1);
+                        point.setIsAbsolute(meterAboveSea.isChecked());
+                        point.setIsGroundFloor(groundFloor.isChecked());
+
+                        Button save = (Button) findViewById(R.id.save_measure_point_button);
+                        pointRepository.add(point);
+
+                        Log.v("YANAT", "Add a point");
                     }
                 })
                         .setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
@@ -80,13 +112,27 @@ public class DetailActivity extends AppCompatActivity {
                         })
                 ;
 
+
+
+
                 AlertDialog dialog = builder.create();
                 dialog.show();
+
+
+
             }
         });
+    }
 
+    private List<Point> getPoints(){
 
+        final List<Point> points = pointRepository.findAll();
+        int i;
+        for (i = 0; i < points.size(); i++) {
+            Log.v("YANAT", Float.toString(points.get(i).getHeight()));
+        }
 
+        return points;
     }
 
 }
---
+++ app/src/main/java/ch/zhaw/moba/yanat/view/PointAdapter.java
@@ -0,0 +1,95 @@
+package ch.zhaw.moba.yanat.view;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ch.zhaw.moba.yanat.DetailActivity;
+import ch.zhaw.moba.yanat.MainActivity;
+import ch.zhaw.moba.yanat.R;
+import ch.zhaw.moba.yanat.domain.model.Point;
+import ch.zhaw.moba.yanat.domain.model.Project;
+import ch.zhaw.moba.yanat.domain.repository.PointRepository;
+import ch.zhaw.moba.yanat.domain.repository.ProjectRepository;
+
+/**
+ * Created by michael on 07.03.16.
+ * src: http://code.tutsplus.com/tutorials/getting-started-with-recyclerview-and-cardview-on-android--cms-23465
+ */
+public class PointAdapter extends RecyclerView.Adapter<PointAdapter.PointViewHolder> {
+
+    private final List<Point> points;
+
+    public PointAdapter(List<Point> points){
+        this.points = points;
+    }
+
+    public static class PointViewHolder extends RecyclerView.ViewHolder {
+        protected View view;
+        protected Point currentPoint;
+
+        protected PointRepository pointRepository;
+
+        TextView title;
+        TextView height;
+        TextView comment;
+        TextView create_date;
+        TextView tstamp;
+
+        PointViewHolder(final View itemView) {
+            super(itemView);
+            this.view = itemView;
+
+            //TODO 0 -> projectid anpassen
+            this.pointRepository = new PointRepository(itemView.getContext(), 0);
+
+            height = (TextView)itemView.findViewById(R.id.input_measure_point_height);
+            comment = (TextView)itemView.findViewById(R.id.input_measure_point_comment);
+        }
+
+        protected DetailActivity getDetailActivity() {
+            return ((DetailActivity)this.view.getContext());
+        }
+
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public PointViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.project_list_item, parent, false);
+        v.setClickable(true);
+        PointViewHolder vh = new PointViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(PointViewHolder pointViewHolder, int i) {
+        pointViewHolder.currentPoint = points.get(i);
+
+        pointViewHolder.title.setText(points.get(i).getTitle());
+        pointViewHolder.comment.setText(points.get(i).getComment());
+        pointViewHolder.height.setText(""+points.get(i).getHeight());
+        pointViewHolder.create_date.setText("Erstellt: " + points.get(i).getCreateDateString());
+        pointViewHolder.tstamp.setText("Bearbeitet: " + points.get(i).getTstampString());
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return points.size();
+    }
+
+}
